180295f8574be3c313f35e3731a6676c89948f60
Touhidul Shawan <touhidulshawan@gmail.com>
2020-11-05 00:13:18 +0600

    hide API key

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,30 +3,29 @@ import "../assets/css/App.min.css";
 import axios from "axios";
 import SearchBar from "../components/SearchBar";
 import ImageLists from "../components/ImageLists";
+
 class App extends Component {
   state = {
     searchKetword: "",
-    imageData: []
+    imageData: [],
   };
 
-  inputChangeHandler = event => {
+  inputChangeHandler = (event) => {
     const searchValue = event.target.value;
     this.setState({ searchKetword: searchValue });
   };
 
-  formSubmitHandler = event => {
+  formSubmitHandler = (event) => {
     event.preventDefault();
     axios
       .get("https://api.unsplash.com/search/photos", {
         params: { query: this.state.searchKetword },
         headers: {
-          Authorization:
-            "Client-ID f3f95c2a970cf32315f88d54860d7ca8d00f782fe39ddabf292ae8e2a0e086b7"
-        }
+          Authorization: `Client-ID ${process.env.REACT_APP_API_KEY}`,
+        },
       })
-      .then(response => {
+      .then((response) => {
         this.setState({ imageData: response.data.results });
-        console.log(this.state.imageData);
       });
   };
 
